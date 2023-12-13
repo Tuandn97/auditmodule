b6983f999394b6e91ddee542a45496cd0a66f20d
tuandao <ngoctuan.dao@my.jcu.edu.au>
2023-12-14 00:18:33 +0800

    Create Title component

---
+++ src/components/pagetitle/PageTitle.jsx
@@ -0,0 +1,9 @@
+import './pagetitle.scss'
+const PageTitle = ({children}) =>{
+    return (
+        <div className="title">
+            {children}
+        </div>
+    )
+}
+export default PageTitle;
