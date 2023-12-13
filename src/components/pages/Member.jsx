c45b998c64982391186d618fafd33845cac39ac0
tuandao <ngoctuan.dao@my.jcu.edu.au>
2023-12-14 00:19:44 +0800

    Call API, Fetch data and create table of Auditor member (fake api)

--- src/components/pages/Member.jsx
+++
@@ -1,4 +0,0 @@
-const Member = () => {
-    return <div>This is Member page</div>
-}
-export default Member
---
+++ src/components/pages/member/Member.jsx
@@ -0,0 +1,41 @@
+import './member.scss'
+import { useEffect, useState } from "react";
+import PageTitle from "../../pagetitle/PageTitle";
+
+const Member = () => {
+    const [members, setMembers] = useState([]);
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then((response) => response.json())
+            .then((json) => {
+                setMembers(json);
+            });
+    }, []);
+
+    return (
+        <div>
+            <PageTitle>Member</PageTitle>
+            <table className="member__table">
+                <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                </tr>
+                </thead>
+                <tbody>
+                {members.map((member) => (
+                    <tr key={member.id}>
+                        <td>{member.id}</td>
+                        <td>{member.name}</td>
+                        <td>{member.email}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default Member;
