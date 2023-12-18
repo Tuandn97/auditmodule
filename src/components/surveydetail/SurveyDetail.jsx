2a3fbe984f5f265c250c43339ab6c1e5227fe23b
tuandao <ngoctuan.dao@my.jcu.edu.au>
2023-12-18 14:46:28 +0800

    update the dashboard

--- src/components/surveydetail/SurveyDetail.jsx
+++ src/components/surveydetail/SurveyDetail.jsx
@@ -1,6 +1,36 @@
-const SurveyDetail = () => {
+import React from 'react';
+import { Card, CardContent, Typography, Button } from '@material-ui/core';
+import './surveydetail.scss';
+
+const SurveyDetailPage = ({onClickBack, surveyData}) => {
     return (
-        <p>This is a survey detail component</p>
-    )
-}
-export default SurveyDetail
+        <div className="survey__detail__wrapper">
+            <div className="survey__detail__heading__wrapper">
+                <Button variant="contained" color="primary" onClick={() => onClickBack()}>Back</Button>
+                <Typography variant="h4" component="h1" gutterBottom>
+                    Survey Detail
+                </Typography>
+                <Button className="survey__detail__header__button" variant="contained" color="primary">New Audit</Button>
+            </div>
+
+            <div className="survey__detail__card__container">
+                {surveyData.map((questionItem, index) => (
+                    <Card key={index} variant="outlined" className="survey__detail__card__content">
+                        <CardContent>
+                            <Typography variant="h6" component="div" gutterBottom className="survey__detail__card__content__question">
+                                {questionItem.question}
+                            </Typography>
+                            {questionItem.answers.map((answer, answerIndex) => (
+                                <Typography key={answerIndex} variant="body1" component="div" className="survey__detail__card__content__answer">
+                                    {answer}
+                                </Typography>
+                            ))}
+                        </CardContent>
+                    </Card>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default SurveyDetailPage;
--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,10 +1,42 @@
 import PageTitle from "../../components/pagetitle/PageTitle";
 import { useEffect, useState } from "react";
 import "./dashboard.scss";
-import SurveyDetail from "../../components/surveydetail/SurveyDetail"
+import SurveyDetail from "../../components/surveydetail/SurveyDetail";
+
+const surveyData = [
+    {
+        question: 'Question 1:',
+        answers: ['Answer 1a', 'Answer 1b', 'Answer 1c']
+    },
+    {
+        question: 'Question 2:',
+        answers: ['Answer 2a', 'Answer 2b', 'Answer 2c']
+    },
+    {
+        question: 'Question 3:',
+        answers: ['Answer 3a', 'Answer 3b', 'Answer 3c']
+    },
+    {
+        question: 'Question 3:',
+        answers: ['Answer 3a', 'Answer 3b', 'Answer 3c']
+    },
+    {
+        question: 'Question 3:',
+        answers: ['Answer 3a', 'Answer 3b', 'Answer 3c']
+    },
+    {
+        question: 'Question 3:',
+        answers: ['Answer 3a', 'Answer 3b', 'Answer 3c']
+    },
+    {
+        question: 'Question 3:',
+        answers: ['Answer 3a', 'Answer 3b', 'Answer 3c']
+    }
+];
 
 const Dashboard = () => {
     const [surveys, setSurveys] = useState([]);
+    const [selectedSurvey, setSelectedSurvey] = useState(false);
 
     useEffect(() => {
         fetch("https://mocki.io/v1/e21487a2-9fed-48d9-9910-48ac2f7ae519")
@@ -22,26 +54,41 @@ const Dashboard = () => {
             });
     }, []);
 
-    return (
-        <div>
-            <PageTitle>Dashboard</PageTitle>
-            {surveys.map((survey) => (
-                <div key={survey.id} className="survey__card">
-                    <div className="survey__card__content">
-                        <div className="survey__card__info">
-                            <h4>{survey.surveyName}</h4>
-                            <p>{survey.description}</p>
-                        </div>
-                        <div
-                            className={`survey__card__status ${survey.status.toLowerCase()}`}
-                        >
-                            {survey.status}
-                        </div>
-                    </div>
-                </div>
-            ))}
+    const handleSurveyClick = (survey) => {
+        setSelectedSurvey(survey);
+    };
 
-            <SurveyDetail/>
+    return (
+        <div className="dashboard__container">
+            <PageTitle>Dashboard</PageTitle>
+
+            {selectedSurvey ? (
+                <div className="dashboard__survey__detail">
+                    <SurveyDetail surveyData={selectedSurvey} onClickBack={setSelectedSurvey} />
+                </div>
+            ) : (
+                <div className="dashboard__survey__list">
+                    {surveys.map((survey) => (
+                        <div
+                            key={survey.id}
+                            className="survey__card"
+                            onClick={() => handleSurveyClick(surveyData)}
+                        >
+                            <div className="survey__card__content">
+                                <div className="survey__card__info">
+                                    <h4>{survey.surveyName}</h4>
+                                    <p>{survey.description}</p>
+                                </div>
+                                <div
+                                    className={`survey__card__status ${survey.status.toLowerCase()}`}
+                                >
+                                    {survey.status}
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            )}
         </div>
     );
 };
